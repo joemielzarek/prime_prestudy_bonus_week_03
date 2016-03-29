250785d84871d9ba344f2ee3c6706b164e3dc6dd
Joe Mielzarek <joseph.mielzarek@gmail.com>
2016-03-29 14:44:35 -0500

    first commit

---
+++ functions.js
@@ -0,0 +1,23 @@
+function addTo (x) {
+	return x + 10;
+}
+
+function subtractFrom (x) {
+	return x - 5;
+}
+
+function subtractAgain (x) {
+
+	return x - 2;
+
+}
+
+console.log(addTo(subtractFrom(subtractAgain( 10 ) ) ) ) ;
+
+
+
+
+
+
+
+
